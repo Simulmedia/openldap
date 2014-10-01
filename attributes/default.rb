a45ecbd80bcdd1d27bd67fbfcf3ca4c572f8ff12
Fahd Sultan <fahd@simulmedia.com>
2014-10-01 17:38:30 -0400

    added port config and tls_reqcert attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,6 +18,7 @@
 
 default['openldap']['basedn'] = "dc=localdomain"
 default['openldap']['server'] = "ldap.localdomain"
+default['openldap']['port'] = '389'
 default['openldap']['tls_enabled'] = true
 default['openldap']['pam_password'] = 'md5'
 
